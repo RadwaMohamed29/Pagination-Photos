e9a7e8ed8eb6dbd38d810231288f4b9e43d99628
RadwaMohamed29 <radwamohamed2979@gmail.com>
2022-11-06 00:06:47 -0700

    Prepare Cashing methods to save and get images from coreData Handel Connection using Rechability

---
+++ Pagination Photos/CoreData/LocalDataSource.swift
@@ -0,0 +1,55 @@
+//
+//  LocalDataSource.swift
+//  Pagination Photos
+//
+//  Created by Radwa on 05/11/2022.
+//
+
+import Foundation
+import CoreData
+
+protocol LocalDataSourcable{
+    func getPhotosFromCoreData() throws -> [Photo]
+    func saveToCoreData(title: String,url:String)throws
+}
+class LocalDataSource:LocalDataSourcable{
+    private var context: NSManagedObjectContext!
+    private var entity: NSEntityDescription!
+    init(appDelegate: AppDelegate){
+        context = appDelegate.persistentContainer.viewContext
+        entity = NSEntityDescription.entity(forEntityName: "Photo", in: context)
+      
+    }
+    func saveToCoreData(title: String,url:String)throws{
+        let photo = Photo(entity: entity, insertInto: context)
+        photo.url = url
+        photo.title = title
+
+        do{
+            try context.save()
+
+        }catch let error as NSError{
+            throw error
+        }
+
+    }
+
+    
+    func getPhotosFromCoreData()throws -> [Photo] {
+        var photos : [Photo] = []
+        let fetchRequest = Photo.fetchRequest()
+        do{
+          let photoList = try context.fetch(fetchRequest)
+            for item in photoList {
+                photos.append(item)
+            }
+            return photos
+        }
+        catch let error as NSError{
+            throw error
+            
+        }
+    }
+
+}
+
---
+++ Pagination Photos/Utilities/HandelConnection.swift
@@ -0,0 +1,37 @@
+//
+//  HandelConnection.swift
+//  Pagination Photos
+//
+//  Created by Radwa on 05/11/2022.
+//
+
+import Foundation
+
+import Reachability
+class HandelConnection{
+    static let handelConnection = HandelConnection()
+    var reachability: Reachability?
+    
+    func checkNetworkConnection(complition: @escaping (Bool)-> Void){
+        reachability = try! Reachability()
+        guard let reachability = reachability else {return}
+        reachability.whenReachable = { reachability in
+            if reachability.connection == .wifi {
+                print("Reachable via WiFi")
+                complition(true)
+            } else {
+                print("Reachable via Cellular")
+                complition(true)
+            }
+        }
+        reachability.whenUnreachable = { _ in
+            print("Not reachable")
+            complition(false)
+        }
+        do {
+            try reachability.startNotifier()
+        } catch {
+            print("Unable to start notifier")
+        }
+    }
+}
